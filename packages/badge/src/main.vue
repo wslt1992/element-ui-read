1850f1619b3d2b23c7f462d45f8bfb0c6574c7a6
liutao <465275024@qq.com>
2019-06-18 14:23:14 +0800

    card（卡片）和 badge（徽）

--- packages/badge/src/main.vue
+++ packages/badge/src/main.vue
@@ -2,6 +2,7 @@
   <div class="el-badge">
     <slot></slot>
     <transition name="el-zoom-in-center">
+<!--      隐藏开启，或者 没有内容 都隐藏-->
       <sup
         v-show="!hidden && (content || content === 0 || isDot)"
         v-text="content"
@@ -23,9 +24,13 @@ export default {
   name: 'ElBadge',
 
   props: {
+    // 值可以是数字和字符串
     value: {},
+    // value为值时，max表示最大值：格式（100+）
     max: Number,
+    // 显示格式为小红点
     isDot: Boolean,
+    // 隐藏badge
     hidden: Boolean,
     type: {
       type: String,
@@ -37,11 +42,12 @@ export default {
 
   computed: {
     content() {
+      //  小红点true，只显示小红点
       if (this.isDot) return;
 
       const value = this.value;
       const max = this.max;
-
+      // 显示值，当超过max(假设10)则显示“10+”
       if (typeof value === 'number' && typeof max === 'number') {
         return max < value ? `${max}+` : value;
       }
--- packages/card/src/main.vue
+++ packages/card/src/main.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="el-card" :class="shadow ? 'is-' + shadow + '-shadow' : 'is-always-shadow'">
+<!--    header默认，输出props中的header-->
+<!--    如果有具名插槽header,则显示自定义-->
+<!--    如果都没有，则隐藏-->
     <div class="el-card__header" v-if="$slots.header || header">
       <slot name="header">{{ header }}</slot>
     </div>
@@ -13,8 +16,12 @@
   export default {
     name: 'ElCard',
     props: {
+      // 标题的值
       header: {},
+      // body的样式
       bodyStyle: {},
+      // shadow的值：always、hover或never
+      // 阴影显示时机
       shadow: {
         type: String
       }
